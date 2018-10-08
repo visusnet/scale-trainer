bca4d0d41651a3e479e10492bfe2d600cd011e23
Alexander Müller <alexander.mueller@codecentric.de>
2018-10-08 11:57:09 +0200

    Updates

---
+++ precache-manifest.aebe56af4069555e3e1ad9da9a4bc4a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f4769098b549538b3e9",
+    "url": "/scale-trainer/static/js/runtime~main.0f476909.js"
+  },
+  {
+    "revision": "68f7f0b5f90ef7111341",
+    "url": "/scale-trainer/static/js/main.68f7f0b5.chunk.js"
+  },
+  {
+    "revision": "893804f0fe368f7318d8",
+    "url": "/scale-trainer/static/js/1.893804f0.chunk.js"
+  },
+  {
+    "revision": "68f7f0b5f90ef7111341",
+    "url": "/scale-trainer/static/css/main.007d8c3d.chunk.css"
+  },
+  {
+    "revision": "237a4f45c76d4dbd6f0a38be2ce0d3b0",
+    "url": "/scale-trainer/index.html"
+  }
+];
